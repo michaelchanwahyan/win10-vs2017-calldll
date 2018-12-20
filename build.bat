d95a555532baa41cb49c113e7645d7d2ea5f5c68
michael <michaelchan_wahyan@yahoo.com.hk>
2018-12-21 07:49:42 +0800

    [update] 1. build.bat: add build MathLibrary.dll and copy header to mainprogram location ; 2. include header file MathLibrary.h (the one under mainprogram folder) in mainprogram project files

--- build.bat
+++ build.bat
@@ -1,8 +1,17 @@
 echo off
+pushd MathLibrary
+rm -rf .vs
+rm -rf x64
+rm -rf MathLibrary/x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" MathLibrary.sln /Clean
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" MathLibrary.sln /build Debug /projectconfig Debug
+popd
+
 rm -rf .vs
 rm -rf x64
 rm -rf mainprogram/x64
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" mainprogram.sln /Clean
+cp MathLibrary\MathLibrary\MathLibrary.h .\mainprogram\
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" mainprogram.sln /build Debug /projectconfig Debug
 echo About to execute :
 x64\Debug\mainprogram
