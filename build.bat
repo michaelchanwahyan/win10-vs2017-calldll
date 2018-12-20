76c9f5ac10dd764ce724fec39ed99bfefa9e1d69
michael <michaelchan_wahyan@yahoo.com.hk>
2018-12-21 06:34:25 +0800

    [update] 1. build.bat includes clean solution and execute .exe; 2. walkthrough.pdf added

--- build.bat
+++ build.bat
@@ -2,4 +2,7 @@ echo off
 rm -rf .vs
 rm -rf x64
 rm -rf mainprogram/x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" mainprogram.sln /Clean
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv" mainprogram.sln /build Debug /projectconfig Debug
+echo About to execute :
+x64\Debug\mainprogram
